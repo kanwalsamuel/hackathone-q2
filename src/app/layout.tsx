00578b2e1ada47103d2714145a191c7808ee0e29
kanwalsamuel <160218038+kanwalsamuel@users.noreply.github.com>
2025-01-24 03:15:26 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -178,17 +178,6 @@ import { useState, useEffect } from "react";
 import LoadingAnimation from "./components/loadingAnimation"; // Import the LoadingAnimation component
 
 // Import local fonts
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function RootLayout({
   children,
